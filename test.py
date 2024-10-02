da2bbe3b4a3c1177220619f0fe39dfc3ff4a69c0
Andy8787(布長) <andy8787main@gmail.com>
2024-10-02 23:07:22 +0800

    do nothing

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-def test02():
-    A = 'Thank#John@I@am@fine'
-    B = 'Hello@John'
-    # C = 
-    print(B,A[5]*2) #1
-    print(B[-1]) #2
-    print(A[::4]) #3
-    print(A[3:-3:3]) #4
-    A_len = len(A)
-    print(A[-7::]) #5
-    B = A.split('@')
-    print(B[3]) #6
-test02()
+print("{1} {0}".format("hello", "world"))
